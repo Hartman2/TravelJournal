a055c5140f28e379d58f971d7bae4ff6a5f9c302
Hartman2 <Hartman2@iastate.edu>
2014-04-16 23:05:52 -0500

    Committing someinterface changes

--- TravelJournal/src/TravelJournal/DataBase.java
+++ TravelJournal/src/TravelJournal/DataBase.java
@@ -1,7 +1,53 @@
 package TravelJournal;
 
-public class DataBase {
+public interface DataBase {
 
-	//Adding to origin?
+	/**
+	 * returns a user given a username and password
+	 * @param name
+	 * @param password
+	 * @return - user
+	 */
+	public Users getUser(String name, String password);
+	
+	/**
+	 * returns a user given a username
+	 * @param userName
+	 * @return - user
+	 */
+	public Users getUser(String userName);
+	
+	
+	/**
+	 * Able to register a given user
+	 * @param u
+	 * @return - true if able to store user. false otherwise
+	 */
+	public boolean putUser(Users u);
+	
+	
+	/**
+	 * retrieves a journal given a string name
+	 * 
+	 * @param name
+	 * @return -  journal
+	 */
+	public Journal getJournal(String name);
+	
+	
+	/**
+	 * retrieves a journal given an id
+	 * @param id
+	 * @return - journal
+	 */
+	public Journal getJournal(int id);
+	
+	
+	/**
+	 * stores journal in db
+	 * @param j
+	 * @return - true if journal is stored. false otherwise
+	 */
+	public boolean putJournal(Journal j);
 	
 }
